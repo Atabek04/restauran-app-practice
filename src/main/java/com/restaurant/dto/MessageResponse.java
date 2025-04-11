a5b755604677994e40dc7b45a624b339b6550c4c
Atabek <otabek.shadimatov@gmail.com>
2025-04-11 16:55:47 +0500

    Create MessageResponse DTO with Lombok annotations

---
+++ src/main/java/com/restaurant/dto/MessageResponse.java
@@ -0,0 +1,13 @@
+package com.restaurant.dto;
+
+import lombok.Data;
+
+@Data
+public class MessageResponse {
+
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+}
