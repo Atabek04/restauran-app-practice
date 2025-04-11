9454e43c11a9640dfe546460c11c8a75b721c656
Atabek <otabek.shadimatov@gmail.com>
2025-04-11 17:00:18 +0500

    Create ReservationResponse DTO with Lombok annotations

---
+++ src/main/java/com/restaurant/dto/ReservationResponse.java
@@ -0,0 +1,21 @@
+package com.restaurant.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ReservationResponse {
+
+    private Long id;
+    private String tableId;
+    private String customerId;
+    private LocalDateTime reservationTime;
+
+    public ReservationResponse(Long id, String tableId, String customerId, LocalDateTime reservationTime) {
+        this.id = id;
+        this.tableId = tableId;
+        this.customerId = customerId;
+        this.reservationTime = reservationTime;
+    }
+}
