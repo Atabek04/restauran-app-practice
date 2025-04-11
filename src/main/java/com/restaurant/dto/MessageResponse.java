54c8868b692bfa24e0f511500cdb5b20e4b76039
Atabek <otabek.shadimatov@gmail.com>
2025-04-11 16:29:34 +0500

    Add MessageResponse DTO

---
+++ src/main/java/com/restaurant/dto/MessageResponse.java
@@ -0,0 +1,12 @@
+package com.restaurant.dto;
+
+public class MessageResponse {
+
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    // Getters and Setters
+}
