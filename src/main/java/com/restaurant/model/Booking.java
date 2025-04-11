df063cc9fa4bb89576522738e1776c768d4ae1e0
Atabek <otabek.shadimatov@gmail.com>
2025-04-11 16:55:35 +0500

    Create Booking entity with Lombok annotations

---
+++ src/main/java/com/restaurant/model/Booking.java
@@ -0,0 +1,26 @@
+package com.restaurant.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "bookings")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String tableId;
+
+    private String customerId;
+
+    private LocalDateTime bookingTime;
+}
