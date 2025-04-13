26268661f26e9a739ee0c5ea422908a6bfb96f06
Atabek <otabek.shadimatov@gmail.com>
2025-04-13 08:34:51 +0500

    Update RoleRepository to extend DynamoDBCrudRepository.

--- src/main/java/com/restaurant/repository/RoleRepository.java
+++ src/main/java/com/restaurant/repository/RoleRepository.java
@@ -1,13 +1,13 @@
 package com.restaurant.repository;
 
 import com.restaurant.model.ERole;
-import com.restaurant.model.Role;
+import com.restaurant.model.UserRole;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.socialsignin.spring.data.dynamodb.repository.DynamoDBCrudRepository;
 
 import java.util.Optional;
 
 @EnableScan
-public interface RoleRepository extends DynamoDBCrudRepository<Role, Integer> {
-    Optional<Role> findByName(ERole name);
+public interface RoleRepository extends DynamoDBCrudRepository<UserRole, Integer> {
+    Optional<UserRole> findByName(ERole name);
 }
