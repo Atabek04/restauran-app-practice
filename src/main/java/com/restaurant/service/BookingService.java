910e861ccfe442d795ebd9d8ccd67097750811fb
Atabek <otabek.shadimatov@gmail.com>
2025-04-13 10:34:50 +0500

    Implement BookingService

---
+++ src/main/java/com/restaurant/service/BookingService.java
@@ -0,0 +1,36 @@
+package com.restaurant.service;
+
+import com.restaurant.model.Booking;
+import com.restaurant.repository.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookingService {
+
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    public List<Booking> getAllBookings() {
+        return (List<Booking>) bookingRepository.findAll();
+    }
+
+    public Optional<Booking> getBookingById(Long id) {
+        return bookingRepository.findById(id);
+    }
+
+    public List<Booking> getBookingsByTableId(String tableId) {
+        return bookingRepository.findByTableId(tableId);
+    }
+
+    public Booking saveBooking(Booking booking) {
+        return bookingRepository.save(booking);
+    }
+
+    public void deleteBooking(Long id) {
+        bookingRepository.deleteById(id);
+    }
+}
