a1f798272f552f9f680dab5744a5ee33b801969c
Atabek <otabek.shadimatov@gmail.com>
2025-04-13 08:34:46 +0500

    Update UserRepository to extend DynamoDBCrudRepository.

--- src/main/java/com/restaurant/repository/UserRepository.java
+++ src/main/java/com/restaurant/repository/UserRepository.java
@@ -1,15 +1,15 @@
 package com.restaurant.repository;
 
-import com.restaurant.model.User;
+import com.restaurant.model.AppUser;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.socialsignin.spring.data.dynamodb.repository.DynamoDBCrudRepository;
 
 import java.util.Optional;
 
 @EnableScan
-public interface UserRepository extends DynamoDBCrudRepository<User, Long> {
+public interface UserRepository extends DynamoDBCrudRepository<AppUser, Long> {
 
-    Optional<User> findByEmail(String email);
+    Optional<AppUser> findByEmail(String email);
 
     boolean existsByEmail(String email);
 }
