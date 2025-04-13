564ecb565d129935f71db44fbcd8ed8027a1a236
Atabek <otabek.shadimatov@gmail.com>
2025-04-13 08:34:41 +0500

    Update BookingRepository to extend DynamoDBCrudRepository.

--- src/main/java/com/restaurant/repository/BookingRepository.java
+++ src/main/java/com/restaurant/repository/BookingRepository.java
@@ -1,12 +1,14 @@
 package com.restaurant.repository;
 
-import com.restaurant.model.Booking;
+import com.restaurant.model.Reservation;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 @EnableScan
-public interface BookingRepository extends CrudRepository<Booking, Long> {
-    List<Booking> findByTableId(String tableId);
+public interface BookingRepository extends DynamoDBCrudRepository<Reservation, Long> {
+    List<Reservation> findByTableId(String tableId);
 }
