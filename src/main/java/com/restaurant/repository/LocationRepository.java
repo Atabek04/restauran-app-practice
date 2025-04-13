192f8ee9398b1583e832b33055e1fd94c9b7f9e5
Atabek <otabek.shadimatov@gmail.com>
2025-04-13 08:45:03 +0500

    Update LocationRepository to extend DynamoDBCrudRepository

--- src/main/java/com/restaurant/repository/LocationRepository.java
+++ src/main/java/com/restaurant/repository/LocationRepository.java
@@ -1,9 +1,11 @@
 package com.restaurant.repository;
 
 import com.restaurant.model.Location;
-import org.springframework.data.repository.CrudRepository;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.socialsignin.spring.data.dynamodb.repository.DynamoDBCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LocationRepository extends CrudRepository<Location, Long> {
+@EnableScan
+public interface LocationRepository extends DynamoDBCrudRepository<Location, Long> {
 }
